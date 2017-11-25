fcd0a10c36f64f61e6d318bd865a4681e92f2db0
Mark Wiemer <mwwiemer@outlook.com>
2017-11-25 12:01:36 -0600

    Add Menu scene

---
+++ Assets/Scripts/controller/NavigationController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+/// <summary>
+/// Attach this to buttons that navigate between scenes
+/// </summary>
+public class NavigationController : MonoBehaviour {
+
+    /// <summary>
+    /// Loads the scene named <paramref name="name"/>
+    /// </summary>
+    /// <param name="name"></param>
+	public void LoadScene(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+}
